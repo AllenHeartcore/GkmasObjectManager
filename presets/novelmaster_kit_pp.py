743146c56c20b881c507e030c8e14728e895486a
Ziyuan Chen <ziyuan.chen@columbia.edu>
2026-06-23 04:18:43 -0400

    Deduplicate 'cidol' images in novelmaster_kit post-processing

--- presets/novelmaster_kit_pp.py
+++ presets/novelmaster_kit_pp.py
@@ -1,19 +1,53 @@
+import asyncio
+from argparse import ArgumentParser
+from collections import Counter, defaultdict
 from pathlib import Path
-from sys import argv
 
-cat_instrs = [
-    ("produce/skillcard", lambda s: s.split("_")[-2]),
-]
+from cryptography.hazmat.primitives.hashes import SHA256, Hash
+from tqdm import tqdm
+
+
+def sha256sum(path: Path, prog: tqdm) -> str:
+    data = path.read_bytes()
+    digest = Hash(SHA256())
+    digest.update(data)
+    prog.update(1)
+    return digest.finalize().hex()
+
+
+async def sha256sum_all(paths: list[Path]) -> list[str]:
+    with tqdm(total=len(paths), desc="Hashing files") as prog:
+        return await asyncio.gather(
+            *[asyncio.to_thread(sha256sum, path, prog) for path in paths]
+        )
 
 
 if __name__ == "__main__":
 
-    root = argv[1]
+    parser = ArgumentParser()
+    parser.add_argument("cwd", type=str)
+    parser.add_argument("--dry", action="store_true")
+    args = parser.parse_args()
 
-    for subdir, cat_func in cat_instrs:
-        parent = Path(root, subdir)
+    cwd = Path(args.cwd)
+    paths = list(cwd.rglob("*.png"))
+    hashes = asyncio.run(sha256sum_all(paths))
 
-        for f in parent.iterdir():
-            cat = cat_func(f.name)
-            (parent / cat).mkdir(parents=True, exist_ok=True)
-            f.rename(parent / cat / f.name)
+    hash2path = defaultdict(list)
+    for h, p in zip(hashes, paths):
+        hash2path[h].append(p)
+
+    dup_hashes = [h for h, c in Counter(hashes).items() if c > 1]
+    for dup_hash in dup_hashes:
+
+        dup = hash2path[dup_hash]
+        assert len(dup) == 2
+        assert dup[0].stem.replace("_0-full", "_1-full") == dup[1].stem
+
+        if args.dry:
+            print(f"Would delete {dup[1]}")
+        else:
+            dup[1].unlink()
+            print(f"Deleted {dup[1]}")
+            new_name = dup[0].name.replace("_0-full", "-full")
+            dup[0].rename(dup[0].with_name(new_name))
